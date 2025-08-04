59e3e30d7f9b9fbe75d581828261f3455b24f8c8
Natnael Getachew <natishget@gmail.com>
2025-08-04 03:39:36 -0400

    fixed useClient error

--- client/app/me/page.tsx
+++ client/app/me/page.tsx
@@ -1,29 +1,29 @@
-"use client";
-import axios from "axios";
-import React, { useState } from "react";
+// "use client";
+// import axios from "axios";
+// import React, { useState } from "react";
 
-const Page = () => {
-  const [res, setRes] = useState();
+// const Page = () => {
+//   const [res, setRes] = useState();
 
-  const handleClick = async () => {
-    try {
-      const response: any = await axios.get(
-        "https://easy-buy-qa2t.onrender.com/auth/me",
-        {
-          withCredentials: true,
-        }
-      );
-      setRes(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div className="flex w-screen h-screen flex-col justify-center items-center gap-5">
-      <button onClick={handleClick}>send request</button>
-      <p>response {res}</p>
-    </div>
-  );
-};
+//   const handleClick = async () => {
+//     try {
+//       const response: any = await axios.get(
+//         "https://easy-buy-qa2t.onrender.com/auth/me",
+//         {
+//           withCredentials: true,
+//         }
+//       );
+//       setRes(response);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <div className="flex w-screen h-screen flex-col justify-center items-center gap-5">
+//       <button onClick={handleClick}>send request</button>
+//       <p>response {res}</p>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
--- client/app/redux/page.tsx
+++ client/app/redux/page.tsx
@@ -1,39 +1,39 @@
-"use client";
+// "use client";
 
-import {
-  decrement,
-  increment,
-  incrementByNumber,
-  decrementByNumber,
-  incrementAsync,
-} from "@/state/API/counterSlice";
-import { AppDispatch, RootState } from "@/state/store";
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import {
+//   decrement,
+//   increment,
+//   incrementByNumber,
+//   decrementByNumber,
+//   incrementAsync,
+// } from "@/state/API/counterSlice";
+// import { AppDispatch, RootState } from "@/state/store";
+// import React, { useState } from "react";
+// import { useSelector, useDispatch } from "react-redux";
 
-const Page = () => {
-  const [amount, setAmount] = useState(1);
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch<AppDispatch>();
+// const Page = () => {
+//   const [amount, setAmount] = useState(1);
+//   const count = useSelector((state: RootState) => state.counter.value);
+//   const dispatch = useDispatch<AppDispatch>();
 
-  return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1>{count}</h1>
-      <div className="flex gap-3">
-        <button onClick={() => dispatch(increment())}>increment</button>
-        <button onClick={() => dispatch(decrement())}>decrement</button>
-      </div>
-      <button onClick={() => dispatch(incrementByNumber(5))} className="ml-5">
-        increment by amount
-      </button>
-      <button onClick={() => dispatch(decrementByNumber(5))}>
-        decrement by amount
-      </button>
-      <button onClick={() => dispatch(incrementAsync(10))}>
-        increment Async
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="w-screen h-screen flex flex-col justify-center items-center">
+//       <h1>{count}</h1>
+//       <div className="flex gap-3">
+//         <button onClick={() => dispatch(increment())}>increment</button>
+//         <button onClick={() => dispatch(decrement())}>decrement</button>
+//       </div>
+//       <button onClick={() => dispatch(incrementByNumber(5))} className="ml-5">
+//         increment by amount
+//       </button>
+//       <button onClick={() => dispatch(decrementByNumber(5))}>
+//         decrement by amount
+//       </button>
+//       <button onClick={() => dispatch(incrementAsync(10))}>
+//         increment Async
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
