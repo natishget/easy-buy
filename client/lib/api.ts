2930ebee09624e3b3c84777f4e570ce0eb0c96d1
Natnael Getachew <natishget@gmail.com>
2025-08-04 05:00:53 -0400

    ADD initial migration for postgrsQL

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://easy-buy-qa2t.onrender.com",
+    baseURL: "https://easy-buy-qa2t.onrender.com:3001",
     // withCredentials: true,
 });
 
