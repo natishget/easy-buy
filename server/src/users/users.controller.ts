2e7a680093fd24e161210f2df429a9784c72be90
Natnael Getachew <natishget@gmail.com>
2025-08-08 07:38:40 -0400

    user API on the backend

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post('create')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get('get')
+  findAll() {
+    console.log("getting all users")
+    return this.usersService.findAll();
+  }
+
+  @Get('get/:id')
+  findOne(@Param('id') id: number) {
+    console.log("getting a single user", id)
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch('update:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete('remove:id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) { }
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique(
+      {
+        where: { id: id },
+        select: {
+          id: true,
+          name: true,
+          isSeller: true,
+          email: true,
+          phone: true
+        }
+      }
+    )
+    console.log("response from database", user)
+    if (!user) {
+      throw new NotFoundException("User with this id doesn't exist");
+    }
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
